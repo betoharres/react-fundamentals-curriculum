21293df779a087dc88ec07c1eaf2b9a5547a7eda
Alberto Harres Rocha <albertohrocha@gmail.com>
2016-03-18 16:10:47 -0300

    Remove whitespace

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -7,7 +7,7 @@ var Main = React.createClass({
 
   render: function() {
     return (
-      <div id="main-container" >
+      <div id="main-container">
         <NavBar>
           {this.props.children}
         </NavBar>
